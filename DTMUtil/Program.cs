d9d90978e8ca7eab932ae1b9dea03427686f7d13
Ryan Barton <bartonrd@sce.EIX.com>
2024-04-10 08:32:39 -0700

    created Program.cs

---
+++ DTMUtil/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DTMUtil
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            LoadSubstation loadSubstation = new LoadSubstation();
+            loadSubstation.StartGUI();
+        }
+    }
+}
+
